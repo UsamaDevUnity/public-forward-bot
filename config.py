f01f8cd5d88a975ad48c8bd6e493f65272f20b03
UsamaDevUnity <153515152+UsamaDevUnity@users.noreply.github.com>
2023-12-11 21:32:11 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "21997406"))
+    API_HASH = os.environ.get("API_HASH", "ce2e4cd3c0f25b82a25124912064cd3a")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6690223775:AAF19ZIvXRsLlMtKiZfdw6MFw3rw1yYrO3U")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "")                             
+    OWNER_ID = os.environ.get("OWNER_ID", "USCryptoking_admin")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     SESSION = os.environ.get("SESSION", "")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "cryptokings212"))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "USCryptoKing_bot")
 
 
 def LOGGER(name: str) -> logging.Logger:
